ed0ad34f8995805f110071f96cf8a2c87545976c
Ben Ellison <ellison.ben@gmail.com>
2016-10-12 14:42:44 +0100

    error handling exercises - chapter 8

---
+++ chapter_8/retry.js
@@ -0,0 +1,39 @@
+/*
+Say you have a function primitiveMultiply that, in 50 percent of cases, 
+multiplies two numbers, and in the other 50 percent, raises an exception of 
+type MultiplicatorUnitFailure. Write a function that wraps this clunky function 
+and just keeps trying until a call succeeds, after which it returns the result.
+
+Make sure you handle only the exceptions you are trying to handle.
+*/
+
+// Code to work with
+function MultiplicatorUnitFailure() {}
+
+function primitiveMultiply(a, b) {
+  if (Math.random() < 0.5)
+    return a * b;
+  else
+    throw new MultiplicatorUnitFailure();
+}
+
+
+// My Code
+function reliableMultiply(a, b) {
+    for (;;) {
+        try {
+            return primitiveMultiply(a, b);
+        } catch (e) {
+            if (e instanceof MultiplicatorUnitFailure) {
+                console.log("Trying again.");
+            } else {
+                throw e;
+            }
+        }
+    }  
+}
+
+
+// Test
+console.log(reliableMultiply(8, 8));
+// → 64
