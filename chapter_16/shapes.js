db3901c231e6829a1c54dfecc9679ee7faeca671
Ben Ellison <ellison.ben@gmail.com>
2016-10-24 13:29:04 +0100

    added size functionality and stardrawing with adjustable number of points

--- chapter_16/shapes.js
+++ chapter_16/shapes.js
@@ -34,35 +34,59 @@ function drawTrapezium(context, position) {
     context.stroke();
 }
 
-function drawDiamond(context, position, colour) {
+function drawDiamond(context, position, size, colour) {
     var x = position.x;
     var y = position.y;
     context.beginPath();
-    context.moveTo(x + 75, y + 10);
-    context.lineTo(x + 10, y + 75);
-    context.lineTo(x + 75, y + 140);
-    context.lineTo(x + 140, y + 75);
+    context.moveTo(x + size/2, y);
+    context.lineTo(x, y + size/2);
+    context.lineTo(x + size/2, y + size);
+    context.lineTo(x + size, y + size/2);
     context.closePath();
     context.fillStyle = colour;
     context.fill()
 }
 
-function drawZigZag(context, position) {
+function drawZigZag(context, position, width, height, numberOfZigs) {
     var x = position.x;
     var y = position.y;
     var dir = 1;
+    var dist = height/numberOfZigs;
     context.beginPath();
-    context.moveTo(x + 10, y + 10);
-    for (var down = 20; down < 180; down += 10) {
-        cx.lineTo(x + 75 + 65*dir, y + down);
+    context.moveTo(x, y);
+    for (var down = dist; down <= height; down += dist) {
+        cx.lineTo(x + width/2 + (width/2)*dir, y + down);
         dir = -dir;
     }
     context.stroke();
 }
 
+function drawStar(context, position, size, numberOfPoints, colour) {
+    var x = position.x;
+    var y = position.y;
+    var angle = 2 * Math.PI/numberOfPoints;
+    var radius = size/2;
+    var edgeX = x + size;
+    var edgeY = y + size/2;
+    context.beginPath();
+    context.moveTo(edgeX, edgeY); //starts middle right
+    for (var a = angle; a <= 2*Math.PI; a += angle) {
+        edgeX = x + size/2 + radius * Math.cos(a);
+        edgeY = y + size/2 + radius * Math.sin(a);
+        context.quadraticCurveTo(x + size/2, y + size/2, edgeX, edgeY);
+    }
+    context.closePath();
+    context.fillStyle = colour;
+    context.fill();
+}
+
 //trapezium
 drawTrapezium(cx, {x: 0, y: 0});
 //red diamond 
-drawDiamond(cx, {x: 150, y: 0}, "red");
+drawDiamond(cx, {x: 150, y: 0}, 150, "red");
 //a zigzagging line
-drawZigZag(cx, {x: 300, y: 0});
+drawZigZag(cx, {x: 300, y: 0}, 100, 150, 11);
+//A spiral made up of 100 straight line segments
+
+//A yellow star
+drawStar(cx, {x: 600, y: 0}, 150, 8, "yellow");
