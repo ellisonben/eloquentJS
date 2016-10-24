a596f3084bc9aa3ce4078bb28ef40f0ae36a2b97
Ben Ellison <ellison.ben@gmail.com>
2016-10-24 14:11:09 +0100

    added spiral drawing function forcanvas

--- chapter_16/shapes.js
+++ chapter_16/shapes.js
@@ -19,17 +19,14 @@ tends to make the code needlessly hard to read and modify.
 
 var cx = document.querySelector("canvas").getContext("2d");
 
-//TODO: introduce size into functions 
-//potentially make shapes flush with their container
-
-function drawTrapezium(context, position) {
+function drawTrapezium(context, position, width, height) {
     var x = position.x;
     var y = position.y;
     context.beginPath();
-    context.moveTo(x + 50, y + 10);
-    context.lineTo(x + 10, y + 70);
-    context.lineTo(x + 140, y + 70);
-    context.lineTo(x + 100, y + 10);
+    context.moveTo(x + width/4, y);
+    context.lineTo(x, y + height);
+    context.lineTo(x + width, y + height);
+    context.lineTo(x + width*3/4, y);
     context.closePath();
     context.stroke();
 }
@@ -55,8 +52,29 @@ function drawZigZag(context, position, width, height, numberOfZigs) {
     context.beginPath();
     context.moveTo(x, y);
     for (var down = dist; down <= height; down += dist) {
-        cx.lineTo(x + width/2 + (width/2)*dir, y + down);
+        context.lineTo(x + width/2 + (width/2)*dir, y + down);
         dir = -dir;
+    }
+    context.stroke();
+}
+
+function drawSpiral(context, position, size, whirls, numberOfLines) {
+    var x = position.x;
+    var y = position.y;
+    var angle = 0;
+    var totalAngle = whirls * 2 * Math.PI;
+    var scale = size / (2 * Math.max(
+                        Math.abs(totalAngle * Math.cos(totalAngle)), 
+                        Math.abs(totalAngle * Math.sin(totalAngle))
+                        ));
+    context.beginPath();
+    context.moveTo(x + size/2, y + size/2);
+    for (var i = 0; i < numberOfLines; i++) {
+        angle += totalAngle/numberOfLines
+        context.lineTo(
+                        x + size/2 + scale * angle * Math.cos(angle), 
+                        y + size/2 + scale * angle * Math.sin(angle)
+                        );
     }
     context.stroke();
 }
@@ -80,13 +98,15 @@ function drawStar(context, position, size, numberOfPoints, colour) {
     context.fill();
 }
 
-//trapezium
-drawTrapezium(cx, {x: 0, y: 0});
-//red diamond 
+//A trapezium
+drawTrapezium(cx, {x: 0, y: 0}, 150, 150);
+//A red diamond 
 drawDiamond(cx, {x: 150, y: 0}, 150, "red");
-//a zigzagging line
-drawZigZag(cx, {x: 300, y: 0}, 100, 150, 11);
+//A zigzagging line
+drawZigZag(cx, {x: 300, y: 0}, 150, 150, 11);
 //A spiral made up of 100 straight line segments
-
+drawSpiral(cx, {x: 450, y: 0}, 150, 3, 100);
 //A yellow star
 drawStar(cx, {x: 600, y: 0}, 150, 8, "yellow");
+//A smoother spiral with more whirls
+drawSpiral(cx, {x: 750, y: 0}, 150, 6, 1000);
