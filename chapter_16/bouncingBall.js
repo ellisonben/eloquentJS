976aa1ff906a79b02d14bd4ae5f1e46036a7b7e5
Ben Ellison <ellison.ben@gmail.com>
2016-10-31 21:58:17 +0000

    ball now bouces basically but needs tidying

--- chapter_16/bouncingBall.js
+++ chapter_16/bouncingBall.js
@@ -5,7 +5,64 @@ and bounces off the box’s sides when it hits them.
 */
 
 var cx = document.querySelector("canvas").getContext("2d");
+var ball = new Ball(new Vector(100, 100));
 
+// Vector constructor
+function Vector(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+Vector.prototype.plus = function(other) {
+    return new Vector(this.x + other.x, this.y + other.y);
+};
+
+Vector.prototype.times = function(factor) {
+    return new Vector(this.x * factor, this.y * factor);
+};
+
+// Ball constructor
+function Ball(position) {
+    this.position = position;
+    this.speed = new Vector(0, 100);
+}
+
+Ball.prototype.act = function(step) {
+    var newPosition = this.position.plus(this.speed.times(step));
+    if (newPosition.x > 400 || newPosition.x < 0) {
+        this.speed = this.speed.times(-1);
+    } else if (newPosition.y > 400 || newPosition.y < 0) {
+        this.speed = this.speed.times(-1); // needs to be improved
+    } else {
+        this.position = newPosition;
+    }
+    
+}
+
+// Updates Ball Position
+function updateBallPosition() {
+    
+}
+
+
+// Draw functions
+function drawBall(centreX, centreY) {
+    cx.beginPath();
+    // arc(centreX centreY radius startAngle endAngle)
+    cx.arc(centreX, centreY, 5, 0, 7);
+    cx.fill();
+}
+
+function clearCanvas() {
+    cx.clearRect(0, 0, 400, 400);
+}
+
+function drawBox() {
+    cx.strokeRect(0, 0, 400, 400);
+}
+
+
+// Animation
 var lastTime = null;
 function frame(time) {
     if (lastTime != null) {
@@ -17,26 +74,15 @@ function frame(time) {
 requestAnimationFrame(frame);
 
 function updateAnimation(step) {
-    // work out position of ball
-    //vectors
-    //speed 
-    //lastPosition
     
+    ball.act(step);
+        
+    clearCanvas();
     
-    // clear canvas
-    cx.clearRect(0, 0, 400, 400);
-    // draw box
-    cx.strokeRect(0, 0, 400, 400);
-    // drawBall(centreX, centreY)
-    drawBall(100, 100);
+    drawBox();
     
+    drawBall(ball.position.x, ball.position.y);
 }
 
-function drawBall(centreX, centreY) {
-    cx.beginPath();
-    // arc(centreX centreY radius startAngle endAngle)
-    cx.arc(centreX, centreY, 5, 0, 7);
-    cx.fill();
-}
 
-function clearCanvas();
+
